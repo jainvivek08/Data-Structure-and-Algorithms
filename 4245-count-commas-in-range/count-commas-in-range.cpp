fcc0d6f539a4a1af55127fb50399602c772ed0b4
Vivek Jain <jainvivek1218@gmail.com>
2026-09-08 14:14:53 +0530

    Time: 11 ms (7.62%) | Memory: 8.6 MB (15.51%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.cpp
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -4,6 +4,10 @@ public:
         int count=0;
         for(int i=1000;i<=n;i++){
             count++;
+            if(i >=1000000)
+                count++;
+            if(i >=1000000000)
+                count++;
         }
         return count;
     }
